f3166039b2ca9bf620a689727a557c69d131847a
Etta Egbe Joseph <ettaegbe40@gmail.com>
2024-05-24 07:49:16 +0100

    initial commit

---
+++ src/stores/auth.ts
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia';
+import { router } from '@/router';
+import { fetchWrapper } from '@/utils/helpers/fetch-wrapper';
+import axios from '@/utils/axios';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}/users`;
+
+export const useAuthStore = defineStore({
+    id: 'auth',
+    state: () => ({
+        // initialize state from local storage to enable user to stay logged in
+        // @ts-ignore
+        user: JSON.parse(localStorage.getItem('user')),
+        returnUrl: null
+    }),
+    actions: {
+        async login(email: string, password: string) {
+            const user = await axios.post(`/connect`, { email, password });
+
+            // update pinia state
+            this.user = user;
+            // store user details and jwt in local storage to keep user logged in between page refreshes
+            localStorage.setItem('user', JSON.stringify(user));
+            // redirect to previous url or default to home page
+            router.push(this.returnUrl || '/dashboards/modern');
+        },
+        logout() {
+            this.user = null;
+            localStorage.removeItem('user');
+            router.push('/');
+        }
+    }
+});
