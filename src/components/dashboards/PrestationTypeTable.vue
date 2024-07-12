f35b023df62a72745b90a65d572d287ef6bb2cc3
Etta Egbe Joseph <ettaegbe40@gmail.com>
2024-07-12 20:44:37 +0100

    addition of a dashboard

--- src/components/dashboards/PrestationTypeTable.vue
+++ src/components/dashboards/PrestationTypeTable.vue
@@ -52,7 +52,7 @@ store.fetchItems(props.options).then((response) => {
                 </thead>
                 <tbody>
                     <tr v-for="item in items" :key="item.id" class="month-item">
-                        <td class="text-no-wrap">
+                        <td>
                             {{ item.name }}
                         </td>
                         <td class="text-no-wrap" v-for="year in years">
